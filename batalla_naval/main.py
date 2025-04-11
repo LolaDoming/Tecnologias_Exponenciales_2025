2b36b36c643b30f8d32361b73dbe5bf25955ec02
DOMINGUEZ LOLA <49375012@ort>
2025-04-11 10:38:11 -0300

    Prueba Loucim Profe

---
+++ batalla_naval/main.py
@@ -0,0 +1,13 @@
+
+EsIgualACien: bool = True
+NumeroDecimal: float = 1.5
+Numero: int = 100
+  
+def ReescribirBool(VariableAManipular: bool): 
+    VariableAManipular = False
+
+
+
+
+if(Numero != 100):
+    ReescribirBool(EsIgualACien)
